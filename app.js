ec51d1e875e96bcfb0a311963cea0b141b5a2a1c
mhsjlw <mhsjlw@aol.com>
2015-08-27 16:58:04 -0400

    Implemented commands

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var mcServer=require("./index");
 
 var settings = require('./config/settings');
+var commands = require('./config/commands');
 
 var options = {
   motd: settings.motd,
   'max-players': settings.maxPlayers,
   port: settings.port,
   'online-mode': settings.onlineMode,
-  gameMode:settings.gameMode
+  gameMode:settings.gameMode,
+  commands: commands
 };
 
 mcServer.createMCServer(options);
