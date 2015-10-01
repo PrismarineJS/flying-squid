fd883eaa54d5a6100b782d14cd51e7c07dfac5c8
F6CF <Creeplays@users.noreply.github.com>
2015-10-01 18:15:13 +0500

    Return basic.js

---
+++ examples/basic.js
@@ -0,0 +1,12 @@
+var mcServer=require("..");
+
+mcServer.createMCServer({
+  motd: "Basic flying-squid server",
+  'max-players': 10,
+  port: 25565,
+  'online-mode': true,
+  gameMode:0,
+  commands: {},
+  logging:false,
+  modpe:false
+});
