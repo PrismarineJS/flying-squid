4b7acbf00ec35d284cd6f91812090d2c134863b8
Romain Beaumont <romain.rom1@gmail.com>
2015-10-08 23:07:29 +0200

    increase the sleep

--- src/lib/playerPlugins/login.js
+++ src/lib/playerPlugins/login.js
@@ -81,7 +81,7 @@ function inject(serv,player)
       })
       .reduce((acc,{chunkX,chunkZ})=>
          acc
-          .then(() => sleep(10))
+          .then(() => sleep(100))
           .then(() => serv.world.getColumn(chunkX,chunkZ))
           .then((column) => sendChunk(chunkX,chunkZ,column))
       ,Promise.resolve());
