cc3977e41b7046ff0b8b40ac499f79ad4a10eb88
JWo1F <savegod1@gmail.com>
2015-10-09 12:27:15 +0300

    World Generation

--- src/lib/serverPlugins/settings.js
+++ src/lib/serverPlugins/settings.js
@@ -5,5 +5,5 @@ module.exports=inject;
 function inject(serv,settings)
 {
   serv.gameMode=settings.gameMode;
-  serv.spawnPoints=[new vec3(6,51,6),new vec3(3,51,6),new vec3(8,51,6)];
+  serv.spawnPoints=[new vec3(6,138,6),new vec3(3,138,6),new vec3(8,138,6)];
 }
