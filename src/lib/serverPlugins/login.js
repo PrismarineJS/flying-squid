3f0c099c42458e5b0b5b026d75d84ba2e194b117
Romain Beaumont <romain.rom1@gmail.com>
2015-10-09 19:46:14 +0200

    generate an uuidv3 in offline mode, fix #68

--- src/lib/serverPlugins/login.js
+++ src/lib/serverPlugins/login.js
@@ -2,12 +2,18 @@ var path = require('path');
 var requireIndex = require('requireindex');
 var playerPlugins = requireIndex(path.join(__dirname,'..', 'playerPlugins'));
 var Player=require("../player");
+var UUID = require('uuid-1345');
 
 module.exports = inject;
 
 function inject(serv,options)
 {
   serv._server.on('login', function (client) {
+    if(!options["online-mode"])
+      client.uuid=UUID.v3({
+        namespace: UUID.namespace.dns,
+        name: client.username
+      });
     var player=new Player();
     player._client=client;
     for(var pluginName in playerPlugins) {
