d7aa0bca835da6f2cff36c423b3e55fb8084f4f9
Romain Beaumont <romain.rom1@gmail.com>
2015-11-06 20:50:29 +0100

    handle player set view distance, fix #76

--- src/lib/playerPlugins/settings.js
+++ src/lib/playerPlugins/settings.js
@@ -4,4 +4,7 @@ function inject(serv,player)
 {
   player.gameMode=serv.gameMode;
   player.spawnPoint=serv.getSpawnPoint();
+  player._client.on('settings',({viewDistance}) => {
+    player.view=viewDistance;
+  });
 }
