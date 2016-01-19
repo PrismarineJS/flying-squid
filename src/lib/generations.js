ef50d796d609354e38b7246a54347a85dcb94220
Romain Beaumont <romain.rom1@gmail.com>
2016-01-19 23:12:30 +0100

    move diamond-square to its own package

--- src/lib/generations.js
+++ src/lib/generations.js
@@ -1,6 +1,6 @@
 module.exports = {
   'grass_field':require("./worldGenerations/grass_field"),
-  'diamond_square':require("./worldGenerations/diamond_square"),
+  'diamond_square':require("diamond-square"),
   'superflat':require("./worldGenerations/superflat"),
   'all_the_blocks':require("./worldGenerations/all_the_blocks"),
   'nether':require("./worldGenerations/nether")
