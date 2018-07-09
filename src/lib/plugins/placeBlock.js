453669f22a815cc73598f1b73937df6443297ffa
Deudly <deudlymandame@gmail.com>
2018-07-09 17:22:50 +0200

    Fix #353 -- Helditem issue fixed
    More info at the issue
    Ref #353

--- src/lib/plugins/placeBlock.js
+++ src/lib/plugins/placeBlock.js
@@ -14,47 +14,63 @@ module.exports.player = function (player, serv, {version}) {
   const blocks = require('minecraft-data')(version).blocks
 
   player._client.on('block_place', ({direction, location} = {}) => {
-    const heldItem = player.inventory.slots[36 + player.heldItemSlot]
-    if (direction === -1 || heldItem.type === -1 || !blocks[heldItem.type]) return
+    let heldItem = player.inventory.slots[36 + player.heldItemSlot]
+    let playSound
+    if (heldItem) {
+      playSound = true
+      if (direction === -1 || heldItem.type === -1 || !blocks[heldItem.type]) return
+    } else {
+      playSound = false
+      heldItem = {
+        type: -1,
+        metadata: ''
+      }
+    }
     const referencePosition = new Vec3(location.x, location.y, location.z)
     const directionVector = directionToVector[direction]
     const placedPosition = referencePosition.plus(directionVector)
-    player.behavior('placeBlock', {
-      direction: directionVector,
-      heldItem: heldItem,
-      id: heldItem.type,
-      damage: heldItem.metadata,
-      position: placedPosition,
-      reference: referencePosition,
-      playSound: true,
-      sound: 'dig.' + (materialToSound[blocks[heldItem.type].material] || 'stone')
-    }, ({direction, heldItem, position, playSound, sound, id, damage}) => {
-      if (playSound) {
-        serv.playSound(sound, player.world, placedPosition.clone().add(new Vec3(0.5, 0.5, 0.5)), {
-          pitch: 0.8
-        })
-      }
+    if (heldItem) {
+      player.behavior('placeBlock', {
+        direction: directionVector,
+        heldItem: heldItem,
+        id: heldItem.type,
+        damage: heldItem.metadata,
+        position: placedPosition,
+        reference: referencePosition,
+        playSound
+      }, ({direction, heldItem, position, playSound, id, damage}) => {
+        if (playSound) {
+          const sound = 'dig.' + (materialToSound[blocks[heldItem.type].material] || 'stone')
+          serv.playSound(sound, player.world, placedPosition.clone().add(new Vec3(0.5, 0.5, 0.5)), {
+            pitch: 0.8
+          })
+        }
 
-      if (player.gameMode === 0) { player.inventory.slots[36 + player.heldItemSlot]-- }
+        if (player.gameMode === 0) { player.inventory.slots[36 + player.heldItemSlot]-- }
 
-      if (heldItem.type !== 323) {
-        player.changeBlock(position, id, damage)
-      } else if (direction === 1) {
-        player.setBlock(position, 63, 0)
-        player._client.write('open_sign_entity', {
-          location: position
-        })
-      } else {
-        player.setBlock(position, 68, 0)
-        player._client.write('open_sign_entity', {
-          location: position
-        })
-      }
-    }, async () => {
-      const id = await player.world.getBlockType(placedPosition)
-      const damage = await player.world.getBlockData(placedPosition)
+        if (heldItem.type !== 323) {
+          player.changeBlock(position, id, damage)
+        } else if (direction === 1) {
+          player.setBlock(position, 63, 0)
+          player._client.write('open_sign_entity', {
+            location: position
+          })
+        } else {
+          player.setBlock(position, 68, 0)
+          player._client.write('open_sign_entity', {
+            location: position
+          })
+        }
+      }, async () => {
+        const id = await player.world.getBlockType(placedPosition)
+        const damage = await player.world.getBlockData(placedPosition)
+        player.sendBlock(placedPosition, id, damage)
+      })
+    } else {
+      const id = player.world.getBlockType(placedPosition)
+      const damage = player.world.getBlockData(placedPosition)
       player.sendBlock(placedPosition, id, damage)
-    })
+    }
   })
 }
 
--- src/lib/plugins/useItem.js
+++ src/lib/plugins/useItem.js
@@ -5,13 +5,15 @@ module.exports.player = function (player, serv, {version}) {
 
   player._client.on('block_place', ({direction, location} = {}) => {
     const heldItem = player.inventory.slots[36 + player.heldItemSlot]
-    if (direction === -1 || heldItem.type === -1 || !items[heldItem.type]) return
-    const item = heldItem
+
+    if (heldItem) {
+      if (direction === -1 || heldItem.type === -1 || !items[heldItem.type]) return
+    }
     const referencePosition = new Vec3(location.x, location.y, location.z)
     const directionVector = directionToVector[direction]
     const position = referencePosition.plus(directionVector)
 
-    if (item.name === 'flint_and_steel') { player.use_flint_and_steel(referencePosition, directionVector, position) } else if (item.name === 'spawn_egg') { serv.spawnMob(item.metadata, player.world, position) }
+    if (heldItem && heldItem.name === 'flint_and_steel') { player.use_flint_and_steel(referencePosition, directionVector, position) } else if (heldItem && heldItem.name === 'spawn_egg') { serv.spawnMob(heldItem.metadata, player.world, position) }
   })
 }
 const directionToVector = [new Vec3(0, -1, 0), new Vec3(0, 1, 0), new Vec3(0, 0, -1), new Vec3(0, 0, 1), new Vec3(-1, 0, 0), new Vec3(1, 0, 0)]
