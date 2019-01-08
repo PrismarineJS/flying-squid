17218a5d7916f659f9dd6b8b2c6630eedf4598b3
bit_knox <johanflensmark@gmail.com>
2019-01-08 16:24:21 +0100

    Fixed crash on useItem and placeBlock

--- src/lib/plugins/useItem.js
+++ src/lib/plugins/useItem.js
@@ -5,6 +5,7 @@ module.exports.player = function (player, serv, { version }) {
 
   player._client.on('block_place', ({ direction, location } = {}) => {
     const heldItem = player.inventory.slots[36 + player.heldItemSlot]
+    if(heldItem == undefined) return
     if (direction === -1 || heldItem.type === -1 || !items[heldItem.type]) return
     const item = heldItem
     const referencePosition = new Vec3(location.x, location.y, location.z)
