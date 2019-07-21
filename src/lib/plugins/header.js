32e7b46ce8452d5e81b651e2554c2eabfa11f828
GoogleSites <43725853+GoogleSites@users.noreply.github.com>
2019-07-21 17:31:04 +0200

    Update header.js

--- src/lib/plugins/header.js
+++ src/lib/plugins/header.js
@@ -1,9 +1,9 @@
 module.exports.player = function (player, serv, settings) {
   player.playerlistUpdateText = (header, footer) =>
     player._client.write('playerlist_header', {
-      header: JSON.stringify(header),
+      // header: JSON.stringify(header),
       footer: JSON.stringify(footer)
     })
 
-  player.playerlistUpdateText(settings['player-list-text']['header'], settings['player-list-text']['footer'])
+  // player.playerlistUpdateText(settings['player-list-text']['header'], settings['player-list-text']['footer'])
 }
