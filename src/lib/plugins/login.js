229807444300c93e175e53f9352825c1a50a5578
rrwr <24882208+rrwr@users.noreply.github.com>
2020-04-18 19:36:28 +0300

    Read player NBT file & set player properties
    Now supports setting health, food, gamemode, XP, current hotbar slot used, position, rotation and inventory contents from NBT. The version of `minecraft-data` package has also been updated.

--- src/lib/plugins/login.js
+++ src/lib/plugins/login.js
@@ -1,9 +1,16 @@
 const Vec3 = require('vec3').Vec3
 
 const path = require('path')
+const fs = require('fs')
+const { promisify } = require('util')
+const nbt = require('prismarine-nbt')
 const requireIndex = require('../requireindex')
 const plugins = requireIndex(path.join(__dirname, '..', 'plugins'))
+const convertInventorySlotId = require('../convertInventorySlotId')
 const Command = require('flying-squid').Command
+
+const fsReadFile = promisify(fs.readFile)
+const nbtParse = promisify(nbt.parse);
 
 module.exports.server = function (serv, options) {
   serv._server.on('connection', client =>
@@ -31,21 +38,70 @@ module.exports.server = function (serv, options) {
 }
 
 module.exports.player = function (player, serv, settings) {
-  function addPlayer () {
+  const Item = require('prismarine-item')(settings.version)
+  const mcData = require('minecraft-data')(settings.version)
+
+  let playerSavedInventoryItems = [];
+  let playerSavedPosition = [];
+  let playerSavedRotation = [];
+
+  async function addPlayer() {
     player.type = 'player'
-    player.health = 20
-    player.food = 20
     player.crouching = false // Needs added in prismarine-entity later
     player.op = settings['everybody-op'] // REMOVE THIS WHEN OUT OF TESTING
     player.username = player._client.username
     player.uuid = player._client.uuid
+    try {
+      const playerDataFile = await fsReadFile(`${settings.worldFolder}/playerdata/${player.uuid}.dat`)
+      const playerData = (await nbtParse(playerDataFile)).value;
+      player.health = playerData.Health.value
+      player.food = playerData.foodLevel.value
+      player.gameMode = playerData.playerGameType.value
+      player.xp = playerData.XpTotal.value
+      player.heldItemSlot = playerData.SelectedItemSlot.value
+      playerSavedPosition = playerData.Pos.value.value;
+      playerSavedRotation = playerData.Rotation.value.value;
+      playerSavedInventoryItems = playerData.Inventory.value.value
+    } catch (err) {
+      // No player data file / other error
+      player.health = 20
+      player.food = 20
+      player.heldItemSlot = 0
+    }
     serv.players.push(player)
     serv.uuidToPlayer[player.uuid] = player
-    player.heldItemSlot = 0
     player.loadedChunks = {}
   }
 
-  function sendLogin () {
+  function updateInventory() {
+    playerSavedInventoryItems.forEach((item) => {
+      let theItem
+      const itemName = item.id.value.slice(10)
+      if (mcData.itemsByName[itemName]) {
+        theItem = mcData.itemsByName[itemName]
+      } else {
+        theItem = mcData.blocksByName[itemName]
+      }
+      const newItem = new Item(theItem.id, item.Count.value, item.Damage.value)
+      let slot = convertInventorySlotId.fromNBT(item.Slot.value)
+      player.inventory.updateSlot(slot, newItem)
+    })
+    player._client.write("held_item_slot", {
+      slot: player.heldItemSlot
+    })
+  }
+
+  async function setSavedPosition() {
+    if (playerSavedPosition.length > 0) {
+      player.position.x = playerSavedPosition[0]
+      player.position.y = playerSavedPosition[1]
+      player.position.z = playerSavedPosition[2]
+      player.yaw = playerSavedRotation[0]
+      player.pitch = playerSavedRotation[1]
+    }
+  }
+
+  function sendLogin() {
     // send init data so client will start rendering world
     player._client.write('login', {
       entityId: player.id,
@@ -59,13 +115,13 @@ module.exports.player = function (player, serv, settings) {
     player.position = player.spawnPoint.clone()
   }
 
-  function sendChunkWhenMove () {
+  function sendChunkWhenMove() {
     player.on('move', () => {
       if (!player.sendingChunks && player.position.distanceTo(player.lastPositionChunkUpdated) > 16) { player.sendRestMap() }
     })
   }
 
-  function updateTime () {
+  function updateTime() {
     player._client.write('update_time', {
       age: [0, 0],
       time: [0, serv.time]
@@ -88,7 +144,7 @@ module.exports.player = function (player, serv, settings) {
     player.sendAbilities()
   }
 
-  function fillTabList () {
+  function fillTabList() {
     player._writeOthers('player_info', {
       action: 0,
       data: [{
@@ -119,7 +175,7 @@ module.exports.player = function (player, serv, settings) {
     }), 5000)
   }
 
-  function announceJoin () {
+  function announceJoin() {
     serv.broadcast(serv.color.yellow + player.username + ' joined the game.')
     player.emit('connected')
   }
@@ -149,14 +205,17 @@ module.exports.player = function (player, serv, settings) {
       return
     }
 
-    addPlayer()
+    await addPlayer()
     await player.findSpawnPoint()
     sendLogin()
     player.sendSpawnPosition()
+    await setSavedPosition()
     player.sendSelfPosition()
     player.sendAbilities()
     await player.sendMap()
     player.updateHealth(player.health)
+    player.setXp(player.xp)
+    updateInventory()
 
     updateTime()
     fillTabList()
