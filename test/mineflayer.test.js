74b9de7d78042cc7fa7be4e1378028f2b38534dd
redcarti <ppamperr@gmail.com>
2020-07-03 17:12:13 +0500

    fixed test

--- test/mineflayer.test.js
+++ test/mineflayer.test.js
@@ -311,6 +311,32 @@ squid.supportedVersions.forEach((supportedVersion, i) => {
           expect(data[0]).toEqual('bot')
         })
       })
+
+      function waitMessagePromise(message) {
+        return new Promise((resolve) => {
+          const listener = (msg) => {
+            if (msg.extra[0].text === message) {
+              bot.removeListener('message', listener)
+              resolve()
+            }
+          }
+          bot.on('message', listener)
+        })
+      }
+
+      test('can use /banlist, /ban, /pardon', async () => {
+        await waitLoginMessage(bot)
+        bot.chat('/banlist')
+        await waitMessagePromise('There are 0 total banned players')
+        bot.chat('/ban bot2')
+        await waitMessagePromise("bot2 was banned")
+        bot.chat('/banlist')
+        await waitMessagePromise('There are 1 total banned players:')
+        bot.chat('/pardon bot2')
+        await waitMessagePromise("bot2 is unbanned")
+        bot.chat('/banlist')
+        await waitMessagePromise('There are 0 total banned players')
+      })
     })
   })
 })
