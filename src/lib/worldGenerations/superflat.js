0e173cdba6929afbf707821407513a0b9c350114
GroobleDierne <grooble.dierne@gmail.com>
2020-08-09 16:46:43 +0200

    Fix bad named functions

--- src/lib/worldGenerations/superflat.js
+++ src/lib/worldGenerations/superflat.js
@@ -1,17 +1,17 @@
 const Vec3 = require('vec3').Vec3
 
-function generation(options = {}) {
+function generation (options = {}) {
   const Chunk = require('prismarine-chunk')(options.version)
   const mcData = require('minecraft-data')(options.version)
 
-  let bottomId = options.bottomId || mcData.blocksByName.bedrock.id
-  let middleId = options.middleId || mcData.blocksByName.dirt.id
-  let topId = options.topId || mcData.blocksByName.grass_block.id
-  let topData = options.topData || 1 //by default the data of grass block is 0 which is snowy
-  let middleThickness = options.middleThickness || 3
-  let debug = options.debug || false
+  const bottomId = options.bottomId || mcData.blocksByName.bedrock.id
+  const middleId = options.middleId || mcData.blocksByName.dirt.id
+  const topId = options.topId || mcData.blocksByName.grass_block.id
+  const topData = options.topData || 1 // by default the data of grass block is 0 which is snowy
+  const middleThickness = options.middleThickness || 3
+  const debug = options.debug || false
 
-  function generateChunk() {
+  function generateChunk () {
     const chunk = new Chunk()
     const height = middleThickness + 1
     const DEBUG_POINTS = [new Vec3(0, height, 0), new Vec3(15, height, 0), new Vec3(0, height, 15), new Vec3(15, height, 15)]
@@ -20,11 +20,11 @@ function generation(options = {}) {
         for (let y = 0; y < middleThickness + 2; y++) {
           if (y === 0) {
             chunk.setBlockType(new Vec3(x, y, z), bottomId)
-            if (options.bottomData) chunk.setDataType(new Vec3(x, y, z), options.bottomData)
+            if (options.bottomData) chunk.setBlockData(new Vec3(x, y, z), options.bottomData)
           }
           else if (y < middleThickness + 1) {
             chunk.setBlockType(new Vec3(x, y, z), middleId)
-            if (options.middleData) chunk.setDataType(new Vec3(x, y, z), options.middleData)
+            if (options.middleData) chunk.setBlockData(new Vec3(x, y, z), options.middleData)
           }
           else {
             chunk.setBlockType(new Vec3(x, y, z), topId)
