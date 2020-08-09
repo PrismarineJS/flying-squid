b8cd8eb0b379bce44aaf13ed9df1fcfead3fd463
GroobleDierne <grooble.dierne@gmail.com>
2020-08-09 16:46:05 +0200

    Set block data for grass block

--- src/lib/worldGenerations/grass_field.js
+++ src/lib/worldGenerations/grass_field.js
@@ -10,6 +10,7 @@ function generation ({ version }) {
     for (let x = 0; x < 16; x++) {
       for (let z = 0; z < 16; z++) {
         chunk.setBlockType(new Vec3(x, 50, z), mcData.blocksByName.grass_block.id)
+        cunck.setBlockData(new Vec3(x, 50, z), 1)
         for (let y = 0; y < 256; y++) {
           chunk.setSkyLight(new Vec3(x, y, z), 15)
         }
