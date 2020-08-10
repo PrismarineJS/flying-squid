9ae66e359787f9f305ad0f2db0877c442faab5f6
Grooble Dierne <grooble.dierne@gmail.com>
2020-08-10 15:07:13 +0200

    Improve sky light again

--- src/lib/worldGenerations/superflat.js
+++ src/lib/worldGenerations/superflat.js
@@ -18,20 +18,17 @@ function generation (options = {}) {
     const DEBUG_POINTS = [new Vec3(0, height, 0), new Vec3(15, height, 0), new Vec3(0, height, 15), new Vec3(15, height, 15)]
     for (let x = 0; x < 16; x++) {
       for (let z = 0; z < 16; z++) {
-        for (let y = 0; y < middleThickness + 2; y++) {
+        for (let y = 0; y < 256; y++) {
           if (y === 0) {
             chunk.setBlockType(new Vec3(x, y, z), bottomId)
             if (options.bottomData) chunk.setBlockData(new Vec3(x, y, z), options.bottomData)
           } else if (y < middleThickness + 1) {
             chunk.setBlockType(new Vec3(x, y, z), middleId)
             if (options.middleData) chunk.setBlockData(new Vec3(x, y, z), options.middleData)
-          } else {
+          } else if (y < middleThickness + 2) {
             chunk.setBlockType(new Vec3(x, y, z), topId)
             if (topData) chunk.setBlockData(new Vec3(x, y, z), topData)
           }
-          chunk.setSkyLight(new Vec3(x, y, z), 15)
-        }
-        for (let y = middleThickness + 2; y < 256; y++) {
           chunk.setSkyLight(new Vec3(x, y, z), 15)
         }
       }
