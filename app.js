d8d3b3d1a826b0071fdf1d4491b57030a99728b7
louis03015 <louis.beaumont@gmail.com>
2020-10-18 16:26:12 +0200

    Allow passing config dir by arguments, update CI and documentation

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
+const argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage('Usage: $0 <command> [options]')
+    .help('h')
+    .option('config', {
+      alias: 'c',
+      type: 'string',
+      default: './config',
+      description: 'Configuration directory'
+    })
+    .argv
+
 const mcServer = require('./')
 
-const defaultSettings = require('./config/default-settings')
+const defaultSettings = require(`./config/default-settings`)
 
 let settings
 
 try {
-  settings = require('./config/settings')
+  settings = require(`${argv.config}/settings`)
 
   Object.keys(defaultSettings).forEach(settingKey => {
     if (settings[settingKey] === undefined) {
