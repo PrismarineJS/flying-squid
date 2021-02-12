2d811508a8cbcf5f03c5de159d9cfc32ad73ec64
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-02-13 00:56:32 +0300

    working

--- src/lib/plugins/useItem.js
+++ src/lib/plugins/useItem.js
@@ -2,7 +2,7 @@ module.exports.server = (serv, { version }) => {
   const mcData = require('minecraft-data')(version)
   const mobs = mcData.mobs
 
-  function getEntID(entName) { // 1.9x, 1.10x and 1.11x
+  function getEntID (entName) { // 1.9x, 1.10x and 1.11x
     let foundID = ''
 
     Object.keys(mobs).forEach(mobID => {
@@ -15,7 +15,7 @@ module.exports.server = (serv, { version }) => {
     return foundID
   }
 
-  function getEntIDmc(entName) { // 1.12x [working 2021]
+  function getEntIDmc (entName) { // 1.12x
     let foundID = ''
 
     Object.keys(mobs).forEach(mobID => {
@@ -40,24 +40,24 @@ module.exports.server = (serv, { version }) => {
         }
       }
     } else {
-	if (version === '1.12.2') {
-		serv.onItemPlace('spawn_egg', ({ item, player, placedPosition }) => {
-		serv.spawnMob(getEntIDmc(item.nbt.value.EntityTag.value.id.value), player.world, placedPosition)
-		return { id: -1, data: 0 }
-		})
-	} else {
-		if (version === '1.8.8') {
-			serv.onItemPlace('spawn_egg', ({ item, player, placedPosition }) => {
-			serv.spawnMob(item.metadata, player.world, placedPosition)
-			return { id: -1, data: 0 }
-		})
-		} else {
-			serv.onItemPlace('spawn_egg', ({ item, player, placedPosition }) => {
-			serv.spawnMob(getEntID(item.nbt.value.EntityTag.value.id.value), player.world, placedPosition)
-			return { id: -1, data: 0 }
-		})
-		}
-	}
-	}
+      if (version === '1.12.2') {
+        serv.onItemPlace('spawn_egg', ({ item, player, placedPosition }) => {
+          serv.spawnMob(getEntIDmc(item.nbt.value.EntityTag.value.id.value), player.world, placedPosition)
+          return { id: -1, data: 0 }
+        })
+      } else {
+        if (version === '1.8.8') {
+          serv.onItemPlace('spawn_egg', ({ item, player, placedPosition }) => {
+            serv.spawnMob(item.metadata, player.world, placedPosition)
+            return { id: -1, data: 0 }
+          })
+        } else {
+          serv.onItemPlace('spawn_egg', ({ item, player, placedPosition }) => {
+            serv.spawnMob(getEntID(item.nbt.value.EntityTag.value.id.value), player.world, placedPosition)
+            return { id: -1, data: 0 }
+          })
+        }
+      }
+    }
   })
 }
