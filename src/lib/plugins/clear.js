5842f10a24a415927ae86cfc1e795f068930881d
u9g <43508353+u9g@users.noreply.github.com>
2021-02-15 14:58:51 -0500

    large refactor to remove complexity and reflect changes in sendChatMessage

--- src/lib/plugins/clear.js
+++ src/lib/plugins/clear.js
@@ -1,41 +1,59 @@
 module.exports.server = (serv, { version }) => {
   const mcData = require('minecraft-data')(version)
   const ChatMessage = require('prismarine-chat')(version)
+  const notUdf = x => x !== undefined
+  const playersString = players => players.length > 1 ? `${players.length} players` : `player ${players[0].username}`
   serv.commands.add({
     base: 'clear',
     info: 'Clears items from player(s) inventory.',
     usage: '/clear <player> <item> <maxCount>',
     parse: /^((?:@|\w)+)?(?: (\w+))?(?: (\d+))?$/, // ex: /clear USERNAME_ stone 1
     action (params, ctx) {
-      const playersString = (players) => players.length > 1 ? `${players.length} players` : `player ${players[0].username}`
       const isConsole = Object.entries(ctx).length === 0
       const isOp = isConsole ? true : ctx.player.op
-      const notUdf = (x) => x !== undefined
-      console.log()
+      const [, playerSelector, removeBlockId, removeCount] = params
+      // errors
       if (typeof params === 'string') return `${this.usage}: ${this.info}` // parsing failed
-      if (isConsole && !notUdf(params[1])) return 'A player is required to run this command here' // console
-      if (!isOp && notUdf(params[1])) return 'Command not found'
-      if (notUdf(params[2]) && !mcData.itemsByName[params[2]]) return 'The block given is invalid.'
-      if (notUdf(params[3]) && isNaN(parseInt(params[3]))) return 'The number of blocks to remove given is invalid.'
-      let playerInput
-      if (isConsole) playerInput = [[undefined, serv.getPlayer(params[1])]]
-      else if (notUdf(params[1])) playerInput = Object.entries(serv.parseSelectorString(params[1], ctx.player.position, ctx.player.world))
-      else playerInput = [[undefined, ctx.player]]
-      const players = playerInput.map(p => p[1])
+      if (isConsole && playerSelector === undefined) return 'A player is required to run this command here' // console
+      if (!isOp && notUdf(playerSelector)) return 'Command not found'
+      if (notUdf(removeBlockId) && !mcData.itemsByName[removeBlockId]) return `Unknown item tag'${removeBlockId}'`
+      if (notUdf(removeCount) && isNaN(parseInt(removeCount))) return 'The number of blocks to remove given is invalid.'
+      // player(s) to execute on
+      let players
+      if (isConsole) {
+        players = [serv.getPlayer(playerSelector)]
+      } else if (notUdf(playerSelector)) {
+        players = Object.entries(serv.parseSelectorString(playerSelector, ctx.player.position, ctx.player.world)).map(p => p[1])
+      } else { // no input given (so default to executor)
+        players = [ctx.player]
+      }
       if (players.some(player => player == null)) return "Player given doesn't exist"
-      const id = params[2] ? mcData.itemsByName[params[2]].id : null
+      const id = removeBlockId ? mcData.itemsByName[removeBlockId].id : null
       // also clear held item if players are in survival
-      const blocksCleared = players.reduce((accumulator, player) => {
-        const res = player.inventory.clear(id, params[3] ? parseInt(params[3]) : undefined)
-        return accumulator + res
+      const blocksCleared = players.reduce((totalRemoved, player) => {
+        const currRemoved = player.inventory.clear(id, removeCount ? parseInt(removeCount) : undefined)
+        return totalRemoved + currRemoved
       }, 0)
       if (blocksCleared === 0) return `No items were found on ${playersString(players)}`
       let serverMessage
-      if (!isConsole && players.length > 1) serverMessage = execByUserToUsers(ctx.player.username, ctx.player.uuid, blocksCleared, players.length)
-      else if (!isConsole && players.length === 1) serverMessage = execByUserToUser(ctx.player.username, ctx.player.uuid, blocksCleared, players[0].username, players[0].uuid)
-      else if (isConsole && players.length > 1) serverMessage = execByConsoleToUsers(blocksCleared, players.length)
-      else if (isConsole && players.length === 1) serverMessage = execByConsoleToUser(blocksCleared, players[0].username, players[0].uuid)
-      serv.sendChatMessage(isConsole ? 'Server' : ctx.player.username, isConsole, serverMessage, false)
+      if (isConsole) {
+        if (players.length > 1) {
+          serverMessage = execByConsoleToUsers(blocksCleared, players.length)
+        } else {
+          serverMessage = execByConsoleToUser(blocksCleared, players[0].username, players[0].uuid)
+        }
+      } else {
+        if (players.length > 1) {
+          serverMessage = execByUserToUsers(ctx.player.username, ctx.player.uuid, blocksCleared, players.length)
+        } else {
+          serverMessage = execByUserToUser(ctx.player.username, ctx.player.uuid, blocksCleared, players[0].username, players[0].uuid)
+        }
+      }
+      serv.sendChatMessage(serverMessage, {
+        messageType: 'system',
+        execByUUID: isConsole ? undefined : ctx.player.uuid,
+        isConsole
+      })
       return `Removed ${blocksCleared} items from ${playersString(players)}`
     }
   })
