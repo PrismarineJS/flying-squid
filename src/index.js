90f25e748d360a1ec7f4557e326f7d2bb872af4b
Nihat <73744616+KaffinPX@users.noreply.github.com>
2021-02-22 16:45:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const checkEula = require('./lib/eula');
 
+if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
+  console.error('Your node version is currently', process.versions.node)
+  console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
+  process.exit(1)
+}
+
 const mc = require('minecraft-protocol')
 const EventEmitter = require('events').EventEmitter
 const path = require('path')
