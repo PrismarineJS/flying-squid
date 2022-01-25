e5dd41e8dd6c85ae0c1efdd44196414ce7bf1af0
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-01-25 13:40:08 +0200

    Update onError.js

--- src/lib/plugins/onError.js
+++ src/lib/plugins/onError.js
@@ -1,26 +1,25 @@
 module.exports.player = async function (player, serv) {
-
-  function unhandledRejection(promise){
-     serv.warn('-------------------------------')
-     serv.warn('Please report this flying-squid! This is bug (mabye)')
-     serv.warn('Unhandled rejection warning!');
-     serv.warn('Error: ' + promise)
-     serv.warn('-------------------------------')
-     player.chat("§cAn error happend in flying-squid's code. Please report it to flying-squid")
-     player.chat("§cError: " + promise)
+  function unhandledRejection (promise) {
+    serv.warn('-------------------------------')
+    serv.warn('Please report this flying-squid! This is bug (mabye)')
+    serv.warn('Unhandled rejection warning!')
+    serv.warn('Error: ' + promise)
+    serv.warn('-------------------------------')
+    player.chat("§cAn error happend in flying-squid's code. Please report it to flying-squid")
+    player.chat('§cError: ' + promise)
   }
 
-  function uncaughtException(err){
-     serv.err('-------------------------------')
-     serv.err('Please report this flying-squid! This is bug (mabye)')
-     serv.err('Uncaught exception! Something went wrong!');
-     serv.err('Error (short error message): ' + err)
-     serv.err('Error (full error message): ' + err.stack)
-     serv.err('-------------------------------')
-     player.chat("§cAn error happend in flying-squid's code. Please report it to flying-squid")
-     player.chat("§cError: " + err)
-    }
+  function uncaughtException (err) {
+    serv.err('-------------------------------')
+    serv.err('Please report this flying-squid! This is bug (mabye)')
+    serv.err('Something went wrong!')
+    serv.err('Error (short error message): ' + err)
+    serv.err('Error (full error message): ' + err.stack)
+    serv.err('-------------------------------')
+    player.chat("§cAn error happend in flying-squid's code. Please report it to flying-squid")
+    player.chat('§cError: ' + err)
+  }
 
-    process.on('unhandledRejection', (promise) => { unhandledRejection(promise) })
-    process.on('uncaughtException', err => { uncaughtException(err) });
+  process.on('unhandledRejection', (promise) => { unhandledRejection(promise) })
+  process.on('uncaughtException', err => { uncaughtException(err) })
 }
