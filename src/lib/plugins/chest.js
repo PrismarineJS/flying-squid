0c2ced718688b6fcc1dd97a682d9139c091ae8be
BuildTools <unconfigured@null.spigotmc.org>
2022-02-07 14:37:21 +0300

    Added support for ender chest GUI window and fixed server config

--- src/lib/plugins/chest.js
+++ src/lib/plugins/chest.js
@@ -1,21 +1,23 @@
 const Vec3 = require('vec3').Vec3
 
 module.exports.player = function (player, serv, { version }) {
-  // const mcData = require('minecraft-data')(version)
-  // Interaction with a chest
-  serv.onBlockInteraction('chest', async ({ block, player }) => {
+  const mcData = require('minecraft-data')(version)
+  // Interaction with a container block
+  const chestsBlockInteractionHandler = async ({ block, player }) => {
     if (player.crouching) return
     try {
-      // const id = await player.world.getBlockType(block.position)
+      const id = await player.world.getBlockType(block.position)
       const blockAbove = await player.world.getBlockType(block.position.plus(new Vec3(0, 1, 0)))
-      // const blockChest = mcData.blocksByName.chest
+      const blockChest = mcData.blocksByName.chest
+      // const blockEnderChest = mcData.blocksByName.ender_chest
       serv.playSound('block.chest.open', player.world, block.position, {})
       if (blockAbove) { return }
       // Opening chest GUI window
+      const chestWindowTitle = id === blockChest.id ? { translate: 'container.chest' } : { translate: 'container.enderchest' }
       player._client.write('open_window', {
         windowId: 1,
         inventoryType: 2,
-        windowTitle: JSON.stringify({ translate: 'container.chest' })
+        windowTitle: JSON.stringify(chestWindowTitle)
       })
       // Sending chest content (NOT IMPLEMENTED)
       player._client.write('window_items', {
@@ -27,10 +29,12 @@ module.exports.player = function (player, serv, { version }) {
           { present: false }, { present: false }, { present: false }, { present: false }, { present: false }, { present: false }, { present: false }, { present: false }, { present: false }],
         carriedItem: { present: false }
       })
-
       return true
     } catch (err) {
       setTimeout(() => { throw err }, 0)
     }
-  })
+  }
+  // Registering block interaction handlers
+  serv.onBlockInteraction('chest', chestsBlockInteractionHandler)
+  serv.onBlockInteraction('ender_chest', chestsBlockInteractionHandler)
 }
