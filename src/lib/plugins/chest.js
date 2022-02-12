78f646e8a15c3b9e93a0b3ae0fd84c1938395a56
BuildTools <unconfigured@null.spigotmc.org>
2022-02-12 15:29:01 +0300

    Making the code of chest.js a bit nicer (registration of shulker box block interaction handlers

--- src/lib/plugins/chest.js
+++ src/lib/plugins/chest.js
@@ -1,26 +1,30 @@
 const Vec3 = require('vec3').Vec3
 
 module.exports.player = function (player, serv, { version }) {
+  // Importing necessary libraries
   const mcData = require('minecraft-data')(version)
-  // Interaction with a container block
+  // Getting ALL supported blocks
+  // Chests
+  const blockChest = mcData.blocksByName.chest
+  const blockEnderChest = mcData.blocksByName.ender_chest
+  // TODO: Large chest (NOT IMPLEMENTED)
+  // Shulker boxes
+  let blockShulkerBox = []
+  if (serv.supportFeature('theShulkerBoxes')) {
+    blockShulkerBox = [mcData.blocksByName.shulker_box.id, mcData.blocksByName.red_shulker_box.id,
+      mcData.blocksByName.orange_shulker_box.id, mcData.blocksByName.yellow_shulker_box.id, mcData.blocksByName.lime_shulker_box.id,
+      mcData.blocksByName.green_shulker_box.id, mcData.blocksByName.light_blue_shulker_box.id, mcData.blocksByName.cyan_shulker_box.id,
+      mcData.blocksByName.blue_shulker_box.id, mcData.blocksByName.purple_shulker_box.id, mcData.blocksByName.magenta_shulker_box.id,
+      mcData.blocksByName.brown_shulker_box.id, mcData.blocksByName.gray_shulker_box.id, mcData.blocksByName.light_gray_shulker_box.id,
+      mcData.blocksByName.black_shulker_box.id, mcData.blocksByName.white_shulker_box.id, mcData.blocksByName.pink_shulker_box.id]
+  }
+  // Interaction with a chest-like ontainer block
   const chestsBlockInteractionHandler = async ({ block, player }) => {
     if (player.crouching) return
     try {
       // Getting current block and block above it
       const id = await player.world.getBlockType(block.position)
       const blockAbove = await player.world.getBlockType(block.position.plus(new Vec3(0, 1, 0)))
-      // Getting ALL supported blocks
-      // Chests
-      const blockChest = mcData.blocksByName.chest
-      const blockEnderChest = mcData.blocksByName.ender_chest
-      // TODO: Large chest (NOT IMPLEMENTED)
-      // Shulker boxes
-      const blockShulkerBox = [mcData.blocksByName.shulker_box.id, mcData.blocksByName.red_shulker_box.id,
-        mcData.blocksByName.orange_shulker_box.id, mcData.blocksByName.yellow_shulker_box.id, mcData.blocksByName.lime_shulker_box.id,
-        mcData.blocksByName.green_shulker_box.id, mcData.blocksByName.light_blue_shulker_box.id, mcData.blocksByName.cyan_shulker_box.id,
-        mcData.blocksByName.blue_shulker_box.id, mcData.blocksByName.purple_shulker_box.id, mcData.blocksByName.magenta_shulker_box.id,
-        mcData.blocksByName.brown_shulker_box.id, mcData.blocksByName.gray_shulker_box.id, mcData.blocksByName.light_gray_shulker_box.id,
-        mcData.blocksByName.black_shulker_box.id, mcData.blocksByName.white_shulker_box.id, mcData.blocksByName.pink_shulker_box.id]
       // Playing sound of opening chest
       if (blockAbove) { return }
       serv.playSound('block.chest.open', player.world, block.position, {})
@@ -66,24 +70,11 @@ module.exports.player = function (player, serv, { version }) {
   // Chests
   serv.onBlockInteraction(mcData.blocksByName.chest.name, chestsBlockInteractionHandler)
   serv.onBlockInteraction(mcData.blocksByName.ender_chest.name, chestsBlockInteractionHandler)
+  // TODO: Large chest (NOT IMPLEMENTED)
   // Shulker boxes
   if (serv.supportFeature('theShulkerBoxes')) {
-    serv.onBlockInteraction(mcData.blocksByName.shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.red_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.orange_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.yellow_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.lime_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.green_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.light_blue_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.cyan_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.blue_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.purple_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.magenta_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.brown_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.gray_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.light_gray_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.black_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.white_shulker_box.name, chestsBlockInteractionHandler)
-    serv.onBlockInteraction(mcData.blocksByName.pink_shulker_box.name, chestsBlockInteractionHandler)
+    for (const currentShulkerBoxID of blockShulkerBox) {
+      serv.onBlockInteraction(mcData.blocks[currentShulkerBoxID].name, chestsBlockInteractionHandler)
+    }
   }
 }
