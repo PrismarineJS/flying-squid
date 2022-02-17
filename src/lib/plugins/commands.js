cd6d0b0a1a59c78747a188304dc92794f10e66a3
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-02-17 22:22:54 +0200

    Update commands.js

--- src/lib/plugins/commands.js
+++ src/lib/plugins/commands.js
@@ -68,7 +68,7 @@ module.exports.server = function (serv, { version }) {
     info: 'to bug report',
     usage: '/bug',
     action () {
-      return 'Report bugs / issues here: https://github.com/PrismarineJS/flying-squid/issues'
+      return 'Report bugs or issues here: https://github.com/PrismarineJS/flying-squid/issues'
     }
   })
 
@@ -83,7 +83,7 @@ module.exports.server = function (serv, { version }) {
     action (sel, ctx) {
       const arr = ctx.player ? serv.selectorString(sel, ctx.player.position, ctx.player.world) : serv.selectorString(sel)
       if (ctx.player) ctx.player.chat(JSON.stringify(arr.map(a => a.id)))
-      else serv.log(JSON.stringify(arr.map(a => a.id)))
+      else serv.info(JSON.stringify(arr.map(a => a.id)))
     }
   })
 
