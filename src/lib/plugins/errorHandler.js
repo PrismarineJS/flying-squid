93b08ea3e0ace196d9d832ee749133d882194b44
andriycraft <92477814+andriycraft@users.noreply.github.com>
2022-05-17 22:46:56 +0300

    Create errorHandler.js

---
+++ src/lib/plugins/errorHandler.js
@@ -0,0 +1,27 @@
+module.exports.player = async function (player, serv) {
+  function unhandledRejection (promise) {
+    serv.warn('-------------------------------')
+    serv.warn('Please report this flying-squid! This is bug (mabye)')
+    serv.warn('Unhandled rejection warning!')
+    serv.warn('Error: ' + promise)
+    serv.warn('Report this error here: https://github.com/PrismarineJS/flying-squid/issues')
+    serv.warn('-------------------------------')
+    serv.emit('unhandledRejectionWarning')
+  }
+
+  function uncaughtException (err) {
+    serv.err('-------------------------------')
+    serv.err('Please report this flying-squid! This is bug (mabye)')
+    serv.err('Something went wrong!')
+    serv.err('Error (short error message): ' + err)
+    serv.err('Error (full error message): ' + err.stack)
+    serv.err('Report this error here: https://github.com/PrismarineJS/flying-squid/issues')
+    serv.err('-------------------------------')
+    serv.emit('crash')
+    serv.quit('Server closed due to an error.')
+    process.exit()
+  }
+
+  process.on('unhandledRejection', (promise) => { unhandledRejection(promise) })
+  process.on('uncaughtException', err => { uncaughtException(err) })
+}
