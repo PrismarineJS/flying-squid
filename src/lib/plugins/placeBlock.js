bee6843b470838f76b75fdc63bf9943c643a27f3
Alexander Ivanov <saiv46.dev@gmail.com>
2022-05-20 20:18:44 +0800

    Update placeBlock.js

--- src/lib/plugins/placeBlock.js
+++ src/lib/plugins/placeBlock.js
@@ -109,6 +109,7 @@ module.exports.player = function (player, serv, { version }) {
     const block = await player.world.getBlock(referencePosition)
     block.position = referencePosition
     if (await serv.interactWithBlock({ block, player })) return
+    if (player.gameMode >= 2) return
 
     const heldItem = player.inventory.slots[36 + player.heldItemSlot]
     if (!heldItem || direction === -1 || heldItem.type === -1) return
