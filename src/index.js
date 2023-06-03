29aec2b1604d8eb34a6cea5e0e9d5d0727f73370
Romain Beaumont <romain.rom1@gmail.com>
2023-06-03 20:35:03 +0000

    Update to node 18.0.0

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
+if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 18) {
   console.error('[\x1b[31mCRITICAL\x1b[0m] Node.JS 14 or newer is required')
   console.error('[\x1b[31mCRITICAL\x1b[0m] You can download the new version from https://nodejs.org/')
   console.error(`[\x1b[31mCRITICAL\x1b[0m] Your current Node.JS version is: ${process.versions.node}`)
