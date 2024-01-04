916239e66fbf71cf7be796ab1c9a054190b64b33
Gavin John <gavinnjohn@gmail.com>
2024-01-04 12:54:18 -0600

    Use previous supportFeature data

--- src/lib/supportFeature.js
+++ src/lib/supportFeature.js
@@ -1,17 +1,5 @@
 const features = require('./features')
 
-module.exports = (feature, version) => {
-  for (const { name, versions } of features) {
-    if (feature !== name) continue
 
-    const supportsFeature = versions.some((featureVersion) => {
-      if (featureVersion.endsWith('x') && featureVersion.startsWith(version.slice(0, -2))) return true
-
-      if (featureVersion === version) return true
-
-      return false
-    })
-
-    return supportsFeature
-  };
-}
+module.exports = (feature, version) =>
+  features.some(({ name, versions }) => name === feature && versions.includes(version))
