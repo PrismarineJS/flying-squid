9b2c5f61e0e5cb1cb4797096f278981ca673ae51
Vitaly <vital2580@icloud.com>
2024-01-18 02:21:35 +0530

    tests should be running as before

--- scripts/genTypes.mjs
+++ scripts/genTypes.mjs
@@ -6,3 +6,7 @@ const plugins = fs.readdirSync('./dist/lib/plugins').filter(f => f !== 'index')
 let types = ''
 types = plugins.filter(module => module.endsWith('.d.ts')).map(module => `import "./lib/plugins/${module}"`).join('\n') + '\n' + types
 fs.writeFileSync(targetFile, types, 'utf8')
+
+let indexTs = fs.readFileSync('./dist/index.d.ts', 'utf8')
+indexTs = `import './types';` + indexTs
+fs.writeFileSync('./dist/index.d.ts', indexTs, 'utf8')
